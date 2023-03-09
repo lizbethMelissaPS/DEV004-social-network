117ddc376c9f3f191890531fb414a12ff33ad019
lizbethMelissaPS <melissasud27@gmail.com>
2023-03-09 17:58:25 -0500

    obtimizacion mudulo Welcome

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,30 +1,18 @@
 import { onNavigate } from '../main';
 /* Para que este disponoble en otro lado export */
 export const Welcome = () => {
-  /* UN CONTENEDOR Q CONTENGA A LOS BOTONES */
+  /* UN CONTENEDOR Q CONTENGA A LOS BOTONES+, crear elementos createElement */
   const div = document.createElement('div');
   const img = document.createElement('img');
   img.src = '../images/sonder.png';
-
-  /* crear botones createElement */
-  const buttonLogin = document.createElement('button');
-  const buttonRegister = document.createElement('button');
-
-  /* AGREGAR TEXTO A LOS BOTONES textContent */
-  buttonLogin.textContent = 'Inicia Sesion';
-  buttonRegister.textContent = 'Registrate';
-  // title.textContent = 'somos lo mejor WELCOME';
 
   /* evento a boton */
   img.addEventListener('click', () => {
     onNavigate('/login');
   });
-  buttonRegister.addEventListener('click', () => {
-    onNavigate('/signup');
-  });
 
   /* INSERTA append */
-  div.append(img, buttonLogin, buttonRegister);
+  div.append(img);
 
   return div;
 };
