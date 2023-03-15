28ef2a026df6b9038a8737300ecf42991d458f79
AlexandraValdez <alexandravaldezs@hotmail.com>
2023-03-14 19:53:46 -0500

    firebase conf

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDgqC7nTcJgfUTNDgiwdFfOksjqylHd7Ns',
+  authDomain: 'sonder-11eee.firebaseapp.com',
+  projectId: 'sonder-11eee',
+  storageBucket: 'sonder-11eee.appspot.com',
+  messagingSenderId: '168562133515',
+  appId: '1:168562133515:web:7ace1494350ec17777950e',
+};
