a57f9bebca7e1f5001d8e7fbfc3acbb592e66848
lizbethMelissaPS <melissasud27@gmail.com>
2023-03-25 00:25:51 -0500

    eleminar

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -8,50 +8,49 @@ export const setupPosts = (data) => {
       console.log(post);
       const section = `
         <article class="post-box">
-        <section class="user-box">
-          <aside class="info">
-              <figure class="profile-postbox">
-              <img src="./images/profilepic.jpg" alt="profile pic">
-              </figure>
-            <aside class="auto-layout">
-              <p class="username-post"> dpretswellh </p>
-              <p class="date-post"> 12/03/23 </p>
+          <section class="user-box">
+            <aside class="info">
+                <figure class="profile-postbox">
+                <img src="./images/profilepic.jpg" alt="profile pic">
+                </figure>
+              <aside class="auto-layout">
+                <p class="username-post"> dpretswellh </p>
+                <p class="date-post"> 12/03/23 </p>
+              </aside>
             </aside>
-          </aside>
-          <aside class="options-post">
-            <button id="option" <img src="./images/3 dots.png" alt="three dots"></button>
-            <button class='btn-delete' data-id="${doc.id}">Delete</button>
-            </aside>
-        </section>
+            <aside class="options-post">
+              <button id="option" <img src="./images/3 dots.png" alt="three dots"></button>
+              <button class='btn-delete' data-id="${doc.id}">Delete</button>
+          </section>
 
-        <section class="card">
+          <section class="card">
 
-          <figure class="photo-post">
-            <img src="${post.img}" alt="post" class="img-post">
-          </figure>
-          <section class="description-box">
-            <p class="pp">${post.description}</p>
-            <p class="pp">${post.location}</p>
-          </section>    
-        </section>
+            <figure class="photo-post">
+              <img src="${post.img}" alt="post" class="img-post">
+            </figure>
+            <section class="description-box">
+              <p class="pp">${post.description}</p>
+              <p class="pp">${post.location}</p>
+            </section>  
+          </section>
 
-        <section class="interaction-box">
-          <figure class="item">
-            <img src="./images/like.png" alt="" class="icon-post">
-            <p class="count">156</p>
-          </figure>
-          <figure class="item">
-            <img src="./images/comment.png" alt="" class="icon-post">
-            <p class="count">13</p>
-          </figure>
-          <figure class="item">
-            <img src="./images/bucket.png" alt="" class="icon-post">
-          </figure>
-          <figure class="item">
-            <img src="./images/share.png" alt="" class="icon-post">
-          </figure>
-        </section>
-      </article>
+          <section class="interaction-box">
+            <figure class="item">
+              <img src="./images/like.png" alt="" class="icon-post">
+              <p class="count">156</p>
+            </figure>
+            <figure class="item">
+              <img src="./images/comment.png" alt="" class="icon-post">
+              <p class="count">13</p>
+            </figure>
+            <figure class="item">
+              <img src="./images/bucket.png" alt="" class="icon-post">
+            </figure>
+            <figure class="item">
+              <img src="./images/share.png" alt="" class="icon-post">
+            </figure>
+          </section>
+        </article>
               `;
       html += section;
     });
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,10 @@
 import { getDocs, collection } from 'firebase/firestore';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../firebase/auth.js';
-import { db } from '../firebase/firestore.js';
+import { db, deleteTask } from '../firebase/firestore.js';
 import { onNavigate } from '../main.js';
 import { setupPosts } from '../components/postCard.js';
 // import { auth, db } from '../firebase/config.js';
-import { onGetTasks, deleteTask, getTask } from '../firebase/firestore.js';
 
 export const home = () => {
   /* UN CONTENEDOR Q CONTENGA A LOS BOTONES */
@@ -52,24 +51,6 @@ export const home = () => {
         deleteTask(dataset.id);
       });
     });
-    ///
-    /// EDITAR
-    const btnEdit = postsContainer.querySelectorAll('.btn-edit');
-    btnEdit.forEach((btn) => {
-      btn.addEventListener('click', async ({ target: { dataset } }) => {
-        const doc = await getTask(dataset.id);
-        const task = doc.data();
-
-        taskForm['task-title'].value = task.title;
-        taskForm['task-description'].value = task.description;
-
-        editStatus = true;
-        id = doc.id;
-
-        taskForm['btn-task-save'].innerText = 'Update';
-      });
-    });
-    ///
   });
   ///
 
@@ -91,96 +72,3 @@ export const home = () => {
 
   return div;
 };
-
-/*
-import { async } from 'regenerator-runtime';
-import { onNavigate } from '../main';
-import {
-  saveTask, onGetTasks, deleteTask, getTask, updateTask,
-} from './firebase';
-
-// Para que este disponoble en otro lado export
-let section;
-export const Welcome = () => {
-  // UN CONTENEDOR Q CONTENGA A LOS BOTONES
-  const div = document.createElement('div');
-  section = document.createElement('div');
-  section.innerHTML = `
-  <form id="task-form">
-  <label for="title"> Title:</label>
-  <input type="text" placeholder="task Title" id="task-title">
-
-  <label for="description"> Description: </label>
-  <textarea type="task-description" rows="3" placeholder="task Description" id="task-description"></textarea>
-
-  <button id="btn-task-save">Save</button>
-</form>
-<div id="task-container"></div>
-      `;
-
-  div.append(section);
-
-  const taskForm = section.querySelector('#task-form');
-  const taskContainer = section.querySelector('#task-container');
-  let editStatus = false;
-  let id = '';
-
-  window.addEventListener('DOMContentLoaded', async () => {
-    // const querySnapshot = await getTasks();
-    onGetTasks((querySnapshot) => {
-      let html = '';
-      querySnapshot.forEach((doc) => {
-        const task = doc.data();
-        html += `
-        <div>
-        <h3>${task.title}</h3>
-        <p>${task.description}</p>
-        <button class='btn-delete' data-id="${doc.id}">Delete</button>
-        <button class='btn-edit' data-id="${doc.id}">Edit</button>
-
-        </div>
-        `;
-      });
-      taskContainer.innerHTML = html;
-
-      const btnDelete = taskContainer.querySelectorAll('.btn-delete');
-      btnDelete.forEach((btn) => {
-        btn.addEventListener('click', ({ target: { dataset } }) => {
-          deleteTask(dataset.id);
-        });
-      });
-
-      const btnEdit = taskContainer.querySelectorAll('.btn-edit');
-      btnEdit.forEach((btn) => {
-        btn.addEventListener('click', async ({ target: { dataset } }) => {
-          const doc = await getTask(dataset.id);
-          const task = doc.data();
-
-          taskForm['task-title'].value = task.title;
-          taskForm['task-description'].value = task.description;
-
-          editStatus = true;
-          id = doc.id;
-
-          taskForm['btn-task-save'].innerText = 'Update';
-        });
-      });
-    });
-  });
-
-  taskForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const title = taskForm['task-title'];
-    const description = taskForm['task-description'];
-    //
-    if (!editStatus) {
-      saveTask(title.value, description.value);
-    } else {
-      updateTask(id, { title: title.value, description: description.value });
-      editStatus = false;
-    }
-    taskForm.reset();
-  });
-
-  return div;
-}; */
