ca4b3cdc00d9aaed47c3eefd9cc43590fc3fe5a1
lizbethMelissaPS <melissasud27@gmail.com>
2023-05-02 12:02:31 -0500

    test

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -8,6 +8,7 @@ import { Login } from '../src/pages/Login.js';
 import { login } from '../src/firebase/auth.js';
 import { Welcome } from '../src/pages/Welcome.js';
 import { home } from '../src/pages/Home.js';
+import { createPost } from '../src/pages/CreatePost.js';
 
 jest.mock('../src/router.js');
 jest.mock('../src/firebase/auth.js');
@@ -24,18 +25,60 @@ describe('Los test del home', () => {
     root.id = 'root';
 
     root.append(home());
-    const signupForm = root.querySelector('#signup-form');
-    const username = root.querySelector('#singup-name');
-    const lastname = root.querySelector('#singup-lastname');
-    const email = root.querySelector('#singup-email');
-    const password = root.querySelector('#singup-password');
+    const postsContainer = root.querySelector('.posts');
+
+    expect(postsContainer).not.toBe(null);
+
+    document.body.append(root);
+  });
+
+  it('deberia mostrar el navegador correctamente', () => {
+    const root = document.createElement('div');
+    root.id = 'root';
+
+    root.append(home());
+    const navSelector = root.querySelector('#nav');
+
+    expect(navSelector).not.toBe(null);
+
+    document.body.append(root);
+  });
+  it('deberia mostrar el deletePost correctamente', () => {
+    const root = document.createElement('div');
+    root.id = 'root';
+
+    root.append(home());
+    const btnDelete = root.querySelector('.btn-delete');
+
+    expect(btnDelete).not.toBe(null);
+
+    document.body.append(root);
+  });
+});
+describe('Los test del create post', () => {
+  it('debería ser una función', () => {
+    expect(typeof createPost).toBe('function');
+  });
+
+  it('deberia mostrar el formulario de createPost correctamente', () => {
+    const root = document.createElement('div');
+    root.id = 'root';
+
+    root.append(createPost());
+    const createForm = root.querySelector('#create-form');
+    const like = [];
+    const likeUserId = [];
+    const dt = new Date().toLocaleDateString();
+    const postLocation = root.querySelector('#post-location');
+    const postDescription = root.querySelector('#post-description');
     const submit = root.querySelector('.submit');
 
-    expect(signupForm).not.toBe(null);
-    expect(username).not.toBe(null);
-    expect(lastname).not.toBe(null);
-    expect(email).not.toBe(null);
-    expect(password).not.toBe(null);
+    expect(createForm).not.toBe(null);
+    expect(like).not.toBe(null);
+    expect(likeUserId).not.toBe(null);
+    expect(dt).not.toBe(null);
+    expect(postLocation).not.toBe(null);
+    expect(postDescription).not.toBe(null);
     expect(submit).not.toBe(null);
 
     document.body.append(root);
